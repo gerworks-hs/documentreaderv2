c6ce8f974e4aeab0d7955c31397aac9c99e22065
itsgerliz <gerworks.hs@gmail.com>
2024-07-22 22:11:31 +0200

    Finished and working, some tweaks may come in the future

--- src/main.c
+++ src/main.c
@@ -1,15 +1,26 @@
 //Made by @itsgerliz
-
 #include <stdio.h>
 
-void readDataStream() {}
+FILE *path;
+
+void readDataStream(FILE *stream) {
+	char nextChar;
+	do {
+		fprintf(stdout, "%c", nextChar);
+	} while ((nextChar = fgetc(stream)) != EOF);
+}
 
 int main(int argc, char **argv) {
 	fprintf(stdout, "File to read -> %s\n", argv[1]);
-	if ((FILE *path = fopen(argv[1], "r") == NULL) {
-		fprintf(stderr, "The file path is not valid\n");
+	if ((path = fopen(argv[1], "r")) == NULL) {
+		fprintf(stderr, "The file path is not valid or you do not have right to read it\n");
+		return 1;
 	} else {
 		fprintf(stdout, "OK!, Reading...\n");
+		fprintf(stdout, "--BEGIN OF FILE--\n");
+		readDataStream(path);
+		fprintf(stdout, "--END OF FILE--\n");
+		return 0;
 	}
 }
 
