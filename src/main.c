07ac3df829059660b1d4361d746b998593dd2534
itsgerliz <gerworks.hs@gmail.com>
2024-07-22 21:32:46 +0200

    Finished file path handling

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 //Made by @itsgerliz
+
 #include <stdio.h>
 
+void readDataStream() {}
+
 int main(int argc, char **argv) {
-	for (int i = 1; i < argc; i++) {
-		fprintf(stdout, "File to read -> %s\n", argv[i]);			
-	} 
+	fprintf(stdout, "File to read -> %s\n", argv[1]);
+	if ((FILE *path = fopen(argv[1], "r") == NULL) {
+		fprintf(stderr, "The file path is not valid\n");
+	} else {
+		fprintf(stdout, "OK!, Reading...\n");
+	}
 }
+
+//Made by @itsgerliz
